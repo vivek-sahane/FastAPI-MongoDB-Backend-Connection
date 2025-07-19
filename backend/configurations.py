971f85c3276c0427c09ae7eb2440572929aa77a4
Vivek Sahane <149855531+vivek-sahane@users.noreply.github.com>
2025-07-19 15:51:30 +0530

    Update configurations.py

--- backend/configurations.py
+++ backend/configurations.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://VivekSahane:Vivek%402005@cluster0.lzaksli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://VivekSahane:<password>@cluster0.lzaksli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
